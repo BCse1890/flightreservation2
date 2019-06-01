9e7065cba7525241fc36e2623bb64b5a9d39f311
BCse1890 <bob.currie@fsresearch.co.uk>
2019-06-01 22:59:11 +0100

    Add email itinerary and Pdf  generator

--- src/main/java/com/bobcurrie/flightreservation2/services/ReservationServiceImpl.java
+++ src/main/java/com/bobcurrie/flightreservation2/services/ReservationServiceImpl.java
@@ -7,6 +7,8 @@ import com.bobcurrie.flightreservation2.model.Reservation;
 import com.bobcurrie.flightreservation2.repository.FlightRepository;
 import com.bobcurrie.flightreservation2.repository.PassengerRepository;
 import com.bobcurrie.flightreservation2.repository.ReservationRepository;
+import com.bobcurrie.flightreservation2.util.EmailUtil;
+import com.bobcurrie.flightreservation2.util.PDFGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,12 @@ public class ReservationServiceImpl implements ReservationService {
 
     @Autowired
     ReservationRepository reservationRepository;
+
+    @Autowired
+    PDFGenerator pdfGenerator;
+
+    @Autowired
+    EmailUtil emailUtil;
 
     @Override
     public Reservation bookFlight(ReservationRequest request) {
@@ -41,6 +49,12 @@ public class ReservationServiceImpl implements ReservationService {
         reservation.setCheckedIn(false);
 
         Reservation savedReservation = reservationRepository.save(reservation);
+
+        String filePath = "/Users/Lenovo/JavaProjects/reservations/reservation"
+                + savedReservation.getId() + ".pdf";
+        pdfGenerator.generateItinary(savedReservation,
+                filePath);
+        emailUtil.sendItinerary(passenger.getEmail(), filePath);
 
         return savedReservation;
     }
